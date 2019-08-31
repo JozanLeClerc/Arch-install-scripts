1ddac8d082ff65e5f2b7f7ed56ce6d2f0c9d5299
Rudy Bousset <bousset.rudy@gmail.com>
2019-08-31 14:21:24 +0200

    work in progress

--- arch_install-UEFI.sh
+++ arch_install-UEFI.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
+clear
 echo "#============ WELCOME ============#"
 echo "#                                 #"
 echo "#        Welcome to Joe's         #"
 echo "#           ARCH LINUX            #"
 echo "#      UEFI INSTALL SCRIPT        #"
 echo "#                                 #"
+echo "#   (press any key to begin...)   #"
+echo "#                                 #"
 echo "#=================================#"
-echo
-echo
-echo
+read
+clear
+echo "#========= I. DISK SETUP =========#"
+echo "#                                 #"
+echo "#      Please choose wisely       #"
+echo "#      1. Drive to be used        #"
+echo "#                                 #"
+echo "#=================================#"
+echo && echo
 echo "Please enter letter of the drive on which Arch Linux should be installed:"
 echo "~> /dev/sd_"
 read drivenum
 drive="/dev/sd$drivenum"
-echo $drive
-echo
+clear
+echo "#========= I. DISK SETUP =========#"
+echo "#                                 #"
+echo "#      Please choose wisely       #"
+echo "#      2. swap partion size       #"
+echo "#                                 #"
+echo "#=================================#"
+echo && echo
 echo "Please enter your swap partition disired size:"
 echo "~> _G"
 read swps
-echo
+clear
+echo "#========= I. DISK SETUP =========#"
+echo "#                                 #"
+echo "#      Please choose wisely       #"
+echo "#      3. root partion size       #"
+echo "#                                 #"
+echo "#=================================#"
+echo && echo
 echo "Please enter your root partition disired size:"
 echo "~> __G"
 read rts
@@ -31,15 +53,20 @@ echo "#=========================== CONFIRM THIS IS EXACT =======================
 echo "#                                                                                #"
 echo "#                            DRIVE TO USE: $drive                              #"
 echo "#                                                                                #"
-echo "#      BOOT partiton size > $btsze                  ROOT partition size > $rtsze      #"
+echo "#      BOOT partiton size > $btsze     ROOT partition size > $rtsze                   #"
 echo "#      SWAP partiton size > $swpsze       HOME partition size > all that remains      #"
 echo "#                                                                                #"
 echo "#================================================================================#" 
+echo && echo
 echo "Is that correct?"
 echo "~> [y/n]"
 read answr
 if [[ $answr != y && $answr != Y && $answr != yes && $answr != Yes ]]; then
-				echo "Aborting"
+				echo "Thank you for using Joe's Arch Linux UEFI install scritpt."
+				sleep 1
+				echo "Aborting..."
+				sleep 3
+				clear
 				exit
 fi
 echo "tasseur"
