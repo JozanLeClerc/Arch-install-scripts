34699a96db37d0f3a4c21ca0fe3a439431965057
JozanLeClerc <bousset.rudy@gmail.com>
2019-11-19 11:44:42 +0100

    Better drive bay table

--- joes-arch-install-UEFI.sh
+++ joes-arch-install-UEFI.sh
@@ -137,7 +137,8 @@ while [[ $answr != y && $answr != Y && $answr != yes && $answr != Yes && $answr 
 		echo && echo
 		dn=$(lsblk | grep -c disk)
 		id=1
-		lsblk | grep disk | awk '{print "\033[1;34mDISK", " | ", "SIZE""\033[0m";}{print "\033[1;34m------+------\033[0m";}{print "\033[1;36m"$1 "\033[1;34m   | ", "\033[1;33m"$4;}'
+		echo "${BBLUE}DISK  |  SIZE\n------+--------${END}"
+		lsblk | grep disk | awk '{print "\033[1;36m"$1 "\033[1;34m   | ", "\033[1;33m"$4;}'
 		echo && echo
 		echo -e "${BCYAN}Please choose the ${BYELLOW}drive ${BCYAN}on which Arch Linux shoud be installed:${END}"
 		while [[ $dn != 0 ]]; do
